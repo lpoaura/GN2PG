c4cfd63b46245499b32d4d56a393e8710d7a0744
lpofredc <frederic.cloitre@lpo.fr>
2020-12-22 23:42:09 +0100

    change rtd docs theme to pydata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,6 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
     "sphinx.ext.viewcode",
-    "sphinx_rtd_theme",
 ]
 
 # show todos
@@ -108,7 +107,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
