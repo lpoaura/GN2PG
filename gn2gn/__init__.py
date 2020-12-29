b70feca6b7bb917adc5522f9efb0011d315abd3b
lpofredc <frederic.cloitre@lpo.fr>
2020-12-29 15:32:23 +0100

    Comment logging dict

--- gn2gn/__init__.py
+++ gn2gn/__init__.py
@@ -10,6 +10,7 @@ import coloredlogs
 from pkg_resources import DistributionNotFound, get_distribution
 
 from . import metadata
+from .env import LOGDIR
 
 try:
     dist_name = "gn2gn_client"
@@ -25,40 +26,47 @@ __copyright__ = metadata.copyright
 
 coloredlogs.DEFAULT_FIELD_STYLES["module"] = {"color": "blue"}
 
-my_logging_dict = {
-    "version": 1,
-    "disable_existing_loggers": True,  # set True to suppress existing loggers from other modules
-    "loggers": {"root": {"level": "DEBUG", "handlers": ["console", "file"],},},
-    "formatters": {
-        "colored_console": {
-            "()": "coloredlogs.ColoredFormatter",
-            "format": "%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s",
-            "datefmt": "%H:%M:%S",
-        },
-        "format_for_file": {
-            "format": "%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-    "handlers": {
-        "console": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "colored_console",
-            "stream": "ext://sys.stdout",
-        },
-        "file": {
-            "level": "INFO",
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "format_for_file",
-            "filename": str(Path.home()) + "/tmp/" + __name__ + ".log",
-            "maxBytes": 500000,
-            "backupCount": 5,
-        },
-    },
-}
+# my_logging_dict = {
+#     "version": 1,
+#     "disable_existing_loggers": True,  # set True to suppress existing loggers from other modules
+#     "loggers": {
+#         "root": {
+#             "level": "DEBUG",
+#             "handlers": ["console", "file"],
+#         },
+#     },
+#     "formatters": {
+#         "colored_console": {
+#             "()": "coloredlogs.ColoredFormatter",
+#             "format": "%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s",
+#             "datefmt": "%H:%M:%S",
+#         },
+#         "format_for_file": {
+#             "format": "%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s",
+#             "datefmt": "%Y-%m-%d %H:%M:%S",
+#         },
+#     },
+#     "handlers": {
+#         "console": {
+#             "level": "INFO",
+#             "class": "logging.StreamHandler",
+#             "formatter": "colored_console",
+#             "stream": "ext://sys.stdout",
+#         },
+#         "file": {
+#             "level": "INFO",
+#             "class": "logging.handlers.TimedRotatingFileHandler",
+#             "formatter": "format_for_file",
+#             "filename": str(LOGDIR) + "/" + __name__ + ".log",
+#             "maxBytes": 500000,
+#             "backupCount": 5,
+#         },
+#     },
+# }
 
-logging.config.dictConfig(my_logging_dict)
+# LOGDIR.mkdir(parents=True, exist_ok=True)
+
+# logging.config.dictConfig(my_logging_dict)
 logger = logging.getLogger("transfer_gn")
 
 coloredlogs.install(
